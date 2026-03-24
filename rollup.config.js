d8ef463dff63dffd6ae5a18ef82931cf75b84da9
Manpreet <manpreet.singh@comprotechnologies.com>
2026-03-24 17:49:43 +0530

    fix: build issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,26 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
+
+const tsPlugin = typescript({
+  tsconfig: './tsconfig.build.json'
+});
 
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true
-    },
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.json',
-        tsconfigOverride: {
-          compilerOptions: {
-            declaration: true,
-            declarationDir: 'dist'
-          }
-        }
-      })
+    output: [
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
     ],
-    external: ['@xmldom/xmldom']
-  },
-  {
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'named'
-    },
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.json',
-        tsconfigOverride: {
-          compilerOptions: {
-            declaration: false
-          }
-        }
-      })
-    ],
+    plugins: [tsPlugin],
     external: ['@xmldom/xmldom']
   }
 ];
